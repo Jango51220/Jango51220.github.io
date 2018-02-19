3ab0b552de4386ac9b29314c95ba83924bbe0c5f
Laurent POIRET <lpoiret@kaliop.com>
2018-02-19 17:47:55 +0100

    april sim

---
+++ aprilinternationalsimulation-1519048958657/js/stats.js
@@ -0,0 +1,671 @@
+var stats = {
+    type: "GROUP",
+name: "Global Information",
+path: "",
+pathFormatted: "group_missing-name-b06d1",
+stats: {
+    "name": "Global Information",
+    "numberOfRequests": {
+        "total": "1379",
+        "ok": "1379",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "60",
+        "ok": "60",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "2616",
+        "ok": "2616",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "333",
+        "ok": "333",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "226",
+        "ok": "226",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "315",
+        "ok": "315",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "402",
+        "ok": "402",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "532",
+        "ok": "532",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "1445",
+        "ok": "1445",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 1351,
+        "percentage": 98
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 7,
+        "percentage": 1
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 21,
+        "percentage": 2
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "11.397",
+        "ok": "11.397",
+        "ko": "-"
+    }
+},
+contents: {
+"req_-fr-mon-compte-ad894": {
+        type: "REQUEST",
+        name: "/fr/mon-compte",
+path: "/fr/mon-compte",
+pathFormatted: "req_-fr-mon-compte-ad894",
+stats: {
+    "name": "/fr/mon-compte",
+    "numberOfRequests": {
+        "total": "179",
+        "ok": "179",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "60",
+        "ok": "60",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "1745",
+        "ok": "1745",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "106",
+        "ok": "106",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "209",
+        "ok": "209",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "63",
+        "ok": "63",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "64",
+        "ok": "64",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "89",
+        "ok": "89",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "1168",
+        "ok": "1168",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 174,
+        "percentage": 97
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 3,
+        "percentage": 2
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 2,
+        "percentage": 1
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1.479",
+        "ok": "1.479",
+        "ko": "-"
+    }
+}
+    },"req_-fr-vivre-l-etr-0a69d": {
+        type: "REQUEST",
+        name: "/fr/vivre-l-etranger/assurance-sante-internationale",
+path: "/fr/vivre-l-etranger/assurance-sante-internationale",
+pathFormatted: "req_-fr-vivre-l-etr-0a69d",
+stats: {
+    "name": "/fr/vivre-l-etranger/assurance-sante-internationale",
+    "numberOfRequests": {
+        "total": "218",
+        "ok": "218",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "373",
+        "ok": "373",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "2516",
+        "ok": "2516",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "456",
+        "ok": "456",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "265",
+        "ok": "265",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "401",
+        "ok": "401",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "409",
+        "ok": "409",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "692",
+        "ok": "692",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "1912",
+        "ok": "1912",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 211,
+        "percentage": 97
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 1,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 6,
+        "percentage": 3
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1.802",
+        "ok": "1.802",
+        "ko": "-"
+    }
+}
+    },"req_-fr-mon-compte--51c72": {
+        type: "REQUEST",
+        name: "/fr/mon-compte Redirect 1",
+path: "/fr/mon-compte Redirect 1",
+pathFormatted: "req_-fr-mon-compte--51c72",
+stats: {
+    "name": "/fr/mon-compte Redirect 1",
+    "numberOfRequests": {
+        "total": "179",
+        "ok": "179",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "214",
+        "ok": "214",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "390",
+        "ok": "390",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "230",
+        "ok": "230",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "27",
+        "ok": "27",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "222",
+        "ok": "222",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "228",
+        "ok": "228",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "289",
+        "ok": "289",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "356",
+        "ok": "356",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 179,
+        "percentage": 100
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1.479",
+        "ok": "1.479",
+        "ko": "-"
+    }
+}
+    },"req_-fr-partenaires-3e82a": {
+        type: "REQUEST",
+        name: "/fr/partenaires-connectez-vous-aux-extranets-de-nos-bureaux",
+path: "/fr/partenaires-connectez-vous-aux-extranets-de-nos-bureaux",
+pathFormatted: "req_-fr-partenaires-3e82a",
+stats: {
+    "name": "/fr/partenaires-connectez-vous-aux-extranets-de-nos-bureaux",
+    "numberOfRequests": {
+        "total": "218",
+        "ok": "218",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "263",
+        "ok": "263",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "1412",
+        "ok": "1412",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "304",
+        "ok": "304",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "89",
+        "ok": "89",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "287",
+        "ok": "287",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "294",
+        "ok": "294",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "390",
+        "ok": "390",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "508",
+        "ok": "508",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 217,
+        "percentage": 100
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 1,
+        "percentage": 0
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1.802",
+        "ok": "1.802",
+        "ko": "-"
+    }
+}
+    },"req_-fr-etudier-l-e-22082": {
+        type: "REQUEST",
+        name: "/fr/etudier-l-etranger/quand-le-cout-des-etudes-l-etranger-s-envole",
+path: "/fr/etudier-l-etranger/quand-le-cout-des-etudes-l-etranger-s-envole",
+pathFormatted: "req_-fr-etudier-l-e-22082",
+stats: {
+    "name": "/fr/etudier-l-etranger/quand-le-cout-des-etudes-l-etranger-s-envole",
+    "numberOfRequests": {
+        "total": "192",
+        "ok": "192",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "304",
+        "ok": "304",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "2032",
+        "ok": "2032",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "371",
+        "ok": "371",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "235",
+        "ok": "235",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "319",
+        "ok": "319",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "325",
+        "ok": "325",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "534",
+        "ok": "534",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "1703",
+        "ok": "1703",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 186,
+        "percentage": 97
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 1,
+        "percentage": 1
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 5,
+        "percentage": 3
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1.587",
+        "ok": "1.587",
+        "ko": "-"
+    }
+}
+    },"req_-fr-dd19d": {
+        type: "REQUEST",
+        name: "/fr",
+path: "/fr",
+pathFormatted: "req_-fr-dd19d",
+stats: {
+    "name": "/fr",
+    "numberOfRequests": {
+        "total": "189",
+        "ok": "189",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "404",
+        "ok": "404",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "2616",
+        "ok": "2616",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "460",
+        "ok": "460",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "170",
+        "ok": "170",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "427",
+        "ok": "427",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "440",
+        "ok": "440",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "605",
+        "ok": "605",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "741",
+        "ok": "741",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 188,
+        "percentage": 99
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 1,
+        "percentage": 1
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1.562",
+        "ok": "1.562",
+        "ko": "-"
+    }
+}
+    },"req_-fr-les-bourses-b263d": {
+        type: "REQUEST",
+        name: "/fr/les-bourses-fullbright-pour-etudier-aux-etats-unis",
+path: "/fr/les-bourses-fullbright-pour-etudier-aux-etats-unis",
+pathFormatted: "req_-fr-les-bourses-b263d",
+stats: {
+    "name": "/fr/les-bourses-fullbright-pour-etudier-aux-etats-unis",
+    "numberOfRequests": {
+        "total": "204",
+        "ok": "204",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "303",
+        "ok": "303",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "2285",
+        "ok": "2285",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "366",
+        "ok": "366",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "237",
+        "ok": "237",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "316",
+        "ok": "316",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "323",
+        "ok": "323",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "440",
+        "ok": "440",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "1541",
+        "ok": "1541",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 196,
+        "percentage": 96
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 2,
+        "percentage": 1
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 6,
+        "percentage": 3
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1.686",
+        "ok": "1.686",
+        "ko": "-"
+    }
+}
+    }
+}
+
+}
+
+function fillStats(stat){
+    $("#numberOfRequests").append(stat.numberOfRequests.total);
+    $("#numberOfRequestsOK").append(stat.numberOfRequests.ok);
+    $("#numberOfRequestsKO").append(stat.numberOfRequests.ko);
+
+    $("#minResponseTime").append(stat.minResponseTime.total);
+    $("#minResponseTimeOK").append(stat.minResponseTime.ok);
+    $("#minResponseTimeKO").append(stat.minResponseTime.ko);
+
+    $("#maxResponseTime").append(stat.maxResponseTime.total);
+    $("#maxResponseTimeOK").append(stat.maxResponseTime.ok);
+    $("#maxResponseTimeKO").append(stat.maxResponseTime.ko);
+
+    $("#meanResponseTime").append(stat.meanResponseTime.total);
+    $("#meanResponseTimeOK").append(stat.meanResponseTime.ok);
+    $("#meanResponseTimeKO").append(stat.meanResponseTime.ko);
+
+    $("#standardDeviation").append(stat.standardDeviation.total);
+    $("#standardDeviationOK").append(stat.standardDeviation.ok);
+    $("#standardDeviationKO").append(stat.standardDeviation.ko);
+
+    $("#percentiles1").append(stat.percentiles1.total);
+    $("#percentiles1OK").append(stat.percentiles1.ok);
+    $("#percentiles1KO").append(stat.percentiles1.ko);
+
+    $("#percentiles2").append(stat.percentiles2.total);
+    $("#percentiles2OK").append(stat.percentiles2.ok);
+    $("#percentiles2KO").append(stat.percentiles2.ko);
+
+    $("#percentiles3").append(stat.percentiles3.total);
+    $("#percentiles3OK").append(stat.percentiles3.ok);
+    $("#percentiles3KO").append(stat.percentiles3.ko);
+
+    $("#percentiles4").append(stat.percentiles4.total);
+    $("#percentiles4OK").append(stat.percentiles4.ok);
+    $("#percentiles4KO").append(stat.percentiles4.ko);
+
+    $("#meanNumberOfRequestsPerSecond").append(stat.meanNumberOfRequestsPerSecond.total);
+    $("#meanNumberOfRequestsPerSecondOK").append(stat.meanNumberOfRequestsPerSecond.ok);
+    $("#meanNumberOfRequestsPerSecondKO").append(stat.meanNumberOfRequestsPerSecond.ko);
+}
---
+++ aprilinternationalsimulation-1519050310407/js/stats.js
@@ -0,0 +1,437 @@
+var stats = {
+    type: "GROUP",
+name: "Global Information",
+path: "",
+pathFormatted: "group_missing-name-b06d1",
+stats: {
+    "name": "Global Information",
+    "numberOfRequests": {
+        "total": "4",
+        "ok": "4",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "34",
+        "ok": "34",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "444",
+        "ok": "444",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "197",
+        "ok": "197",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "153",
+        "ok": "153",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "155",
+        "ok": "155",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "250",
+        "ok": "250",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "405",
+        "ok": "405",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "436",
+        "ok": "436",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 4,
+        "percentage": 100
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "4",
+        "ok": "4",
+        "ko": "-"
+    }
+},
+contents: {
+"req_-fr-vivre-l-etr-0a69d": {
+        type: "REQUEST",
+        name: "/fr/vivre-l-etranger/assurance-sante-internationale",
+path: "/fr/vivre-l-etranger/assurance-sante-internationale",
+pathFormatted: "req_-fr-vivre-l-etr-0a69d",
+stats: {
+    "name": "/fr/vivre-l-etranger/assurance-sante-internationale",
+    "numberOfRequests": {
+        "total": "1",
+        "ok": "1",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "444",
+        "ok": "444",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "444",
+        "ok": "444",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "444",
+        "ok": "444",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "444",
+        "ok": "444",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "444",
+        "ok": "444",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "444",
+        "ok": "444",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "444",
+        "ok": "444",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 1,
+        "percentage": 100
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1",
+        "ok": "1",
+        "ko": "-"
+    }
+}
+    },"req_-fr-les-bourses-b263d": {
+        type: "REQUEST",
+        name: "/fr/les-bourses-fullbright-pour-etudier-aux-etats-unis",
+path: "/fr/les-bourses-fullbright-pour-etudier-aux-etats-unis",
+pathFormatted: "req_-fr-les-bourses-b263d",
+stats: {
+    "name": "/fr/les-bourses-fullbright-pour-etudier-aux-etats-unis",
+    "numberOfRequests": {
+        "total": "1",
+        "ok": "1",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "185",
+        "ok": "185",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "185",
+        "ok": "185",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "185",
+        "ok": "185",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "185",
+        "ok": "185",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "185",
+        "ok": "185",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "185",
+        "ok": "185",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "185",
+        "ok": "185",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 1,
+        "percentage": 100
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1",
+        "ok": "1",
+        "ko": "-"
+    }
+}
+    },"req_-fr-mon-compte-ad894": {
+        type: "REQUEST",
+        name: "/fr/mon-compte",
+path: "/fr/mon-compte",
+pathFormatted: "req_-fr-mon-compte-ad894",
+stats: {
+    "name": "/fr/mon-compte",
+    "numberOfRequests": {
+        "total": "1",
+        "ok": "1",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "34",
+        "ok": "34",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "34",
+        "ok": "34",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "34",
+        "ok": "34",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "34",
+        "ok": "34",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "34",
+        "ok": "34",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "34",
+        "ok": "34",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "34",
+        "ok": "34",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 1,
+        "percentage": 100
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1",
+        "ok": "1",
+        "ko": "-"
+    }
+}
+    },"req_-fr-mon-compte--51c72": {
+        type: "REQUEST",
+        name: "/fr/mon-compte Redirect 1",
+path: "/fr/mon-compte Redirect 1",
+pathFormatted: "req_-fr-mon-compte--51c72",
+stats: {
+    "name": "/fr/mon-compte Redirect 1",
+    "numberOfRequests": {
+        "total": "1",
+        "ok": "1",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "124",
+        "ok": "124",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "124",
+        "ok": "124",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "124",
+        "ok": "124",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "124",
+        "ok": "124",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "124",
+        "ok": "124",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "124",
+        "ok": "124",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "124",
+        "ok": "124",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 1,
+        "percentage": 100
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1",
+        "ok": "1",
+        "ko": "-"
+    }
+}
+    }
+}
+
+}
+
+function fillStats(stat){
+    $("#numberOfRequests").append(stat.numberOfRequests.total);
+    $("#numberOfRequestsOK").append(stat.numberOfRequests.ok);
+    $("#numberOfRequestsKO").append(stat.numberOfRequests.ko);
+
+    $("#minResponseTime").append(stat.minResponseTime.total);
+    $("#minResponseTimeOK").append(stat.minResponseTime.ok);
+    $("#minResponseTimeKO").append(stat.minResponseTime.ko);
+
+    $("#maxResponseTime").append(stat.maxResponseTime.total);
+    $("#maxResponseTimeOK").append(stat.maxResponseTime.ok);
+    $("#maxResponseTimeKO").append(stat.maxResponseTime.ko);
+
+    $("#meanResponseTime").append(stat.meanResponseTime.total);
+    $("#meanResponseTimeOK").append(stat.meanResponseTime.ok);
+    $("#meanResponseTimeKO").append(stat.meanResponseTime.ko);
+
+    $("#standardDeviation").append(stat.standardDeviation.total);
+    $("#standardDeviationOK").append(stat.standardDeviation.ok);
+    $("#standardDeviationKO").append(stat.standardDeviation.ko);
+
+    $("#percentiles1").append(stat.percentiles1.total);
+    $("#percentiles1OK").append(stat.percentiles1.ok);
+    $("#percentiles1KO").append(stat.percentiles1.ko);
+
+    $("#percentiles2").append(stat.percentiles2.total);
+    $("#percentiles2OK").append(stat.percentiles2.ok);
+    $("#percentiles2KO").append(stat.percentiles2.ko);
+
+    $("#percentiles3").append(stat.percentiles3.total);
+    $("#percentiles3OK").append(stat.percentiles3.ok);
+    $("#percentiles3KO").append(stat.percentiles3.ko);
+
+    $("#percentiles4").append(stat.percentiles4.total);
+    $("#percentiles4OK").append(stat.percentiles4.ok);
+    $("#percentiles4KO").append(stat.percentiles4.ko);
+
+    $("#meanNumberOfRequestsPerSecond").append(stat.meanNumberOfRequestsPerSecond.total);
+    $("#meanNumberOfRequestsPerSecondOK").append(stat.meanNumberOfRequestsPerSecond.ok);
+    $("#meanNumberOfRequestsPerSecondKO").append(stat.meanNumberOfRequestsPerSecond.ko);
+}
